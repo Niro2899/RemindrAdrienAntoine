759f7ca9b01766f23e91a332dc342ec10aaabfc7
Antoine <antoinebaptistemozart2@gmail.com>
2023-12-12 12:05:26 +0100

    Création d'utilisateur: terminé

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -4,6 +4,8 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
+const path = require('path');
+
 async function index (req, res) {
   res.send('Hello, world!');
   //\todo: appeler fonctions de Prisma & de la vue Handlebars
@@ -11,43 +13,8 @@ async function index (req, res) {
 }
 
 function showLogin (req, res) {
-  res.send(`<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=*, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-      <title>formulaire_inscription</title>
-  </head>
-  <body>
-      <form method="post">
-          <div class="form-group">
-            <label for="nom">Nom</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" name = "name">
-          </div>
-          <div class="form-group">
-            <label for="prenom">Prenom</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" name = "firstname">
-          </div>
-          <div class="form-group">
-            <label for="pseudo">Pseudo</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" name = "pseudo">
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" name = "email" aria-describedby="emailHelp" placeholder="Enter email">
-          </div>
-          <div class="form-group">
-            <label for="mdp">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" name = "password" placeholder="Password">
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-  </body>
-  </html>
-  
-  firstnameUser`);
+  const createUserPath = path.join(__dirname, '..', 'Static', 'Users', 'createUser.html');
+  res.sendFile(createUserPath);
 }
 
 async function create (req, res) {
@@ -62,8 +29,13 @@ async function create (req, res) {
     }
   });
 
-  res.redirect('../Templates/Users/dashboardUser.hbs');
+  res.redirect(req.baseUrl + "/dashboard");
   
 }
 
-module.exports = { index, showLogin, create };
+function showDashboard (req, res) {
+  const dashboardPath = path.join(__dirname, '..', 'Templates', 'Users', 'dashboardUser.hbs');
+  res.sendFile(dashboardPath);
+}
+
+module.exports = { index, showLogin, create, showDashboard };
--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -4,8 +4,11 @@ const rtrUsers = express.Router();
 const userController = require('../Controllers/userController');
 
 rtrUsers.get('/', userController.index);
+
 rtrUsers.get('/create', userController.showLogin);
 rtrUsers.post('/create', userController.create);
+
+rtrUsers.get('/dashboard', userController.showDashboard);
 
 module.exports = { rtrUsers };
 
